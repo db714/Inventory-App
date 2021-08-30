36d79342b304588083046edd70c1075b70064760
db714 <dbfocus1@gmail.com>
2021-08-30 14:22:48 -0500

    -Created Main_Menu Screen -Modified app boot screen -Created actionable buttons for Main screen

--- src/controller/Main_Screen.java
+++ src/controller/Main_Screen.java
@@ -1,34 +1,140 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import model.Inventory;
 import model.Product;
 
 import javax.swing.table.TableColumn;
 import javax.swing.text.TableView;
+import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Main_Screen {
-
-
-  /*  @FXML
-    private TableView<Product> productTableView;
-
-    @FXML
-    private TableColumn<Product,String> idCol;
+// TODO remove (((xmlns="http://javafx.com/javafx/16" from Main_Screen.fxml)))
+public class Main_Screen implements Initializable {
+   /* @FXML
+    private TableView<?> partMainTableview;
 
     @FXML
-    private TableColumn<Product,String> nameCol;
+    private TableColumn<?, ?> partIDMainCol;
 
     @FXML
-    private TableColumn<Product,Integer> levelCol;
+    private TableColumn<?, ?> partNameMainCol;
 
     @FXML
-    private TableColumn<Product,Double> priceCol;
+    private TableColumn<?, ?> partInvMainCol;
+
+    @FXML
+    private TableColumn<?, ?> partPriceMainCol;
+
+    @FXML
+    private TableView<?> prodMainTableview;
+
+    @FXML
+    private TableColumn<?, ?> prodIDMainCol;
+
+    @FXML
+    private TableColumn<?, ?> prodNameMainCol;
+
+    @FXML
+    private TableColumn<?, ?> prodInvMainCol;
+
+    @FXML
+    private TableColumn<?, ?> prodPriceMainCol;
 */
-  // @Override
-   // public void initialize(URL url, ResourceBundle rb){
+    @FXML
+    private Button mainExitBut;
 
-       //productTableView.setItems(Inventory.getAllProducts());
+    @FXML
+    private Button partAddMainBut;
+
+    @FXML
+    private Button partDelMainBut;
+
+    @FXML
+    private Button partModMainBut;
+
+    @FXML
+    private Button prodDelMainBut;
+
+    @FXML
+    private Button prodAddMainBut;
+
+    @FXML
+    private Button prodModMainBut;
+
+    @FXML
+    private TextField partMainTxt;
+
+    @FXML
+    private TextField prodMainTxt;
+
+   /* @FXML
+    void onActionMainExit(ActionEvent event) {
+        System.out.println("This Exits the System");
+    }
+
+    @FXML
+    void onActionPartAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionPartDel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionPartMod(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionProdAdd(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionProdDel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionProdMod(ActionEvent event) {
+
+    }
+*/
+
+   @Override
+   public void initialize(URL url, ResourceBundle rb){
+
+   }
+
+    public void onActionPartAdd(javafx.event.ActionEvent actionEvent) {
+    }
+
+    public void onActionMainExit(ActionEvent actionEvent) {
+        System.out.println("This should exit the program!!!!");
+    }
+
+    public void onActionPartDel(ActionEvent actionEvent) {
+    }
+
+    public void onActionPartMod(ActionEvent actionEvent) {
+    }
+
+    public void onActionProdDel(ActionEvent actionEvent) {
+    }
+
+    public void onActionProdAdd(ActionEvent actionEvent) {
+    }
+
+    public void onActionProdMod(ActionEvent actionEvent) {
+    }
+
+    //productTableView.setItems(Inventory.getAllProducts());
  //   }
 }
